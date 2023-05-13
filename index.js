29badb6469cc5edeadc88d2539c67e2d2665cf9d
Aidin Forooghi <80118914+aidinforooghi@users.noreply.github.com>
2023-05-13 19:26:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const moviesContainer = document.getElementById('movies-container');
